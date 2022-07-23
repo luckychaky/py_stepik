d35731fdb1c85450936c06069874e12773ea4079
luckychaky <vitali.adv@gmail.com>
2022-07-23 22:12:46 +0300

    Create 2_5_11.py

---
+++ 2_5_11.py
@@ -0,0 +1,36 @@
+'''
+Напишите программу, которая принимает на вход список чисел в одной строке и выводит на экран в одну строку значения, которые встречаются в нём более одного раза.
+
+Для решения задачи может пригодиться метод sort списка.
+
+Выводимые числа не должны повторяться, порядок их вывода может быть произвольным.
+
+Sample Input 1:
+4 8 0 3 4 2 0 3
+Sample Output 1:
+0 3 4
+Sample Input 2:
+10
+Sample Output 2:
+Sample Input 3:
+1 1 2 2 3 3
+Sample Output 3:
+1 2 3
+Sample Input 4:
+1 1 1 1 1 2 2 2
+Sample Output 4:
+1 2
+'''
+numbers = [int(i) for i in input().split()]
+list = []
+for number in numbers:
+    ind = numbers.index(number)
+    ind_2 = int(ind) + 1
+    for j in numbers[ind_2:]:
+	      if number == j:
+	          list.append(number)
+list_new = []
+for i in list:
+    if i not in list_new:
+        list_new.append(i)
+print(*list_new)
